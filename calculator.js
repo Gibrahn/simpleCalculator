88470f1c19d47ef5c3dac62ea6a6b33e0c70bf14
Gibrahn <gibrahn.duarte18@tectijuana.edu.mx>
2022-03-07 20:25:28 -0800

    Did changes to the css

--- calculator.js
+++ calculator.js
@@ -3,12 +3,16 @@ function calculation(){
 
     //get values from  the prompt and store them in two varibles
     let number1 = Number(prompt("Enter value 1: "));
+    let number2 = Number(prompt("Enter value 2: "));
     //do the operations and store rhe results in vars(you need four vars)
-    let sum = number1 + 10;
-
+    let sum = number1 + number2;
+    let sub = number1 - number2;
+    let multi = number1 * number2;
+    let div = number1 / number2;
     //display in html
-    document.getElementById("results").innerHTML=
-    
-    `<p> The sum is:${sum}</p>`;
+    document.getElementById("add").innerHTML=`<p> The sum is:${sum}</p>`;
+    document.getElementById("sub").innerHTML=`<p> The substraction is:${sub}</p>`;
+    document.getElementById("multi").innerHTML=`<p> The multiplication is:${multi}</p>`;
+    document.getElementById("div").innerHTML=`<p> The division is:${div}</p>`;
 }
 
